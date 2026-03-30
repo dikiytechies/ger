001a5c2805863f7f3149b7854a2944fcfcfe9c6c
dikiytechies <160992549+dikiytechies@users.noreply.github.com>
2026-03-30 15:56:16 +0300

    huh

--- src/main/java/com/dikiytechies/ger/GerMain.java
+++ src/main/java/com/dikiytechies/ger/GerMain.java
@@ -1,9 +1,11 @@
 package com.dikiytechies.ger;
 
 import com.dikiytechies.ger.init.AddonInits;
+import com.github.standobyte.jojo.action.Action;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -18,11 +20,10 @@ public class GerMain {
 
         AddonInits.addonRegistries(modEventBus);
         AddonInits.vanillaRegistries(modEventBus);
-
-        modEventBus.addListener(this::onFMLCommonSetup);
     }
 
-    private void onFMLCommonSetup(FMLCommonSetupEvent event) {
-        event.enqueueWork(AddonInits::initCommunityAddonsBanList);
+    @SubscribeEvent
+    public static void onFMLRegister(RegistryEvent.Register<Action<?>> event) {
+        AddonInits.initCommunityAddonsBanList();
     }
 }
--- src/main/java/com/dikiytechies/ger/mixin/StandArrowItemMixin.java
+++ src/main/java/com/dikiytechies/ger/mixin/StandArrowItemMixin.java
@@ -16,12 +16,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = StandArrowItem.class, remap = false)
+@Mixin(value = StandArrowItem.class)
 public abstract class StandArrowItemMixin extends ArrowItem {
 
     public StandArrowItemMixin(Properties properties) { super(properties); }
 
-    @Inject(method = "use(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Hand;)Lnet/minecraft/util/ActionResult;", at = @At("HEAD"), cancellable = true, remap = false)
+    @Inject(method = "use(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Hand;)Lnet/minecraft/util/ActionResult;", at = @At("HEAD"), cancellable = true)
     public void use(World world, PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult<ItemStack>> cir) {
         if (!world.isClientSide()) {
             IStandPower power = IStandPower.getPlayerStandPower(player);

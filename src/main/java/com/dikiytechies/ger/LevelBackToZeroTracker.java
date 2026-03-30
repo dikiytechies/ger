60d80b5fef70d885eeae07bbcf322f867e18cb7e
StandoByte <94539965+StandoByte@users.noreply.github.com>
2026-03-30 17:46:31 +0300

    Implemented Back to Zero through cancelling the action click directly

---
+++ src/main/java/com/dikiytechies/ger/LevelBackToZeroTracker.java
@@ -0,0 +1,56 @@
+package com.dikiytechies.ger;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.github.standobyte.jojo.action.stand.effect.StandEffectInstance;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.storage.DimensionSavedDataManager;
+import net.minecraft.world.storage.WorldSavedData;
+
+public class LevelBackToZeroTracker extends WorldSavedData {
+    public static final String ID = "rotp_ger:back_to_zero_effects";
+    public List<StandEffectInstance> activeBackToZeroEffects = new ArrayList<>();
+    
+    public LevelBackToZeroTracker(String id) {
+        super(id);
+    }
+    
+    public void tick() {
+        Iterator<StandEffectInstance> iter = activeBackToZeroEffects.iterator();
+        while (iter.hasNext()) {
+            StandEffectInstance backToZeroEffect = iter.next();
+            boolean removed = backToZeroEffect.toBeRemoved();
+            if (!removed) {
+                LivingEntity gerUser = backToZeroEffect.getStandUser();
+                removed |= gerUser == null || !gerUser.isAlive();
+            }
+            if (removed) {
+                iter.remove();
+            }
+        }
+    }
+    
+    // capabilities are annoying, this is faster to set up
+    public static LevelBackToZeroTracker get(World level) {
+        if (level instanceof ServerWorld) {
+            ServerWorld serverLevel = (ServerWorld) level;
+            DimensionSavedDataManager aaa = serverLevel.getChunkSource().getDataStorage();
+            return aaa.computeIfAbsent(() -> new LevelBackToZeroTracker(ID), ID);
+        }
+        return null;
+    }
+
+    @Override
+    public void load(CompoundNBT nbt) {}
+
+    @Override
+    public CompoundNBT save(CompoundNBT nbt) {
+        return nbt;
+    }
+}
---
+++ src/main/java/com/dikiytechies/ger/mixin/PowerCancelActionClickMixin.java
@@ -0,0 +1,32 @@
+package com.dikiytechies.ger.mixin;
+
+import javax.annotation.Nullable;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.dikiytechies.ger.action.effect.BackToZeroEffect;
+import com.github.standobyte.jojo.action.Action;
+import com.github.standobyte.jojo.action.ActionTarget;
+import com.github.standobyte.jojo.power.IPower;
+import com.github.standobyte.jojo.power.impl.PowerBaseImpl;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.network.PacketBuffer;
+
+@SuppressWarnings("rawtypes")
+@Mixin(PowerBaseImpl.class)
+public abstract class PowerCancelActionClickMixin implements IPower {
+    @Shadow @Final protected LivingEntity user;
+    
+    @Inject(method = "clickAction", at = @At("HEAD"), cancellable = true)
+    public void cancelActionClick(Action<?> action, boolean sneak, ActionTarget target, @Nullable PacketBuffer extraInput, CallbackInfoReturnable<Boolean> cir) {
+        if (BackToZeroEffect.cancelOnAbilityUse(action, this, user)) {
+            cir.setReturnValue(false);
+        }
+    }
+}

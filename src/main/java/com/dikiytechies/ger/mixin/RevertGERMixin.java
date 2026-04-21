3e9a53aa32a8e2180c6ea44afdeb2719ef225219
dikiytechies <160992549+dikiytechies@users.noreply.github.com>
2026-04-22 00:27:46 +0300

    gradle-wadle

--- src/main/java/com/dikiytechies/ger/mixin/RevertGERMixin.java
+++ src/main/java/com/dikiytechies/ger/mixin/RevertGERMixin.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = ResolveEffect.class, remap = false)
+@Mixin(value = ResolveEffect.class)
 public abstract class RevertGERMixin extends StatusEffect {
 
     public RevertGERMixin(EffectType type, int liquidColor) { super(type, liquidColor); }
